7d282abd1ac4556fb1286b1618d046abdc12ac6b
artem.yankovets <Artem.Yankovets@b-next.com>
2017-10-10 13:26:37 +0300

    created simple maven progect

---
+++ src/main/java/com/aya/spring/springdemomaven/SpringdemomavenApplication.java
@@ -0,0 +1,12 @@
+package com.aya.spring.springdemomaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdemomavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdemomavenApplication.class, args);
+	}
+}
