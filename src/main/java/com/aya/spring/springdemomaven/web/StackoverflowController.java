4bcb4812bbfc9da92292613536894bbe59c0763f
ArtemYankovets <ArtemYankovets@gmail.com>
2017-10-12 01:48:35 +0300

    add @CrossOrigin for avoiding error connected with No 'Access-Control-Allow-Origin' header in the requested resource

--- src/main/java/com/aya/spring/springdemomaven/web/StackoverflowController.java
+++ src/main/java/com/aya/spring/springdemomaven/web/StackoverflowController.java
@@ -3,12 +3,14 @@ package com.aya.spring.springdemomaven.web;
 import com.aya.spring.springdemomaven.model.StackoverflowWebsite;
 import com.aya.spring.springdemomaven.service.StackoverflowService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8099")
 @RequestMapping("/api/stackoverflow")
 public class StackoverflowController {
 
