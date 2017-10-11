16b6b742ddc4600a4d606b9fdd9dc27ac9b7f2f6
ArtemYankovets <ArtemYankovets@gmail.com>
2017-10-12 01:51:25 +0300

    remove @Setter. For using Lombok you should Enable annotation processing in IDE (settings -> annotation processing)

--- src/main/java/com/aya/spring/springdemomaven/model/StackoverflowWebsite.java
+++ src/main/java/com/aya/spring/springdemomaven/model/StackoverflowWebsite.java
@@ -3,18 +3,17 @@ package com.aya.spring.springdemomaven.model;
 import lombok.*;
 
 @Getter
-@Setter
 public class StackoverflowWebsite {
     private final String id;
-    private final String iconImageUrl;
     private final String website;
+    private final String iconImageUrl;
     private final String title;
     private final String description;
 
-    public StackoverflowWebsite(String id, String iconImageUrl, String website, String title, String description) {
+    public StackoverflowWebsite(String id, String website, String iconImageUrl, String title, String description) {
         this.id = id;
-        this.iconImageUrl = iconImageUrl;
         this.website = website;
+        this.iconImageUrl = iconImageUrl;
         this.title = title;
         this.description = description;
     }
