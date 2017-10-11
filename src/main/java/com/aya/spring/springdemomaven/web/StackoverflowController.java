cd224c928e61c80bdcd76cef60590c47dbd29f45
ArtemYankovets <ArtemYankovets@gmail.com>
2017-10-11 23:36:44 +0300

    Created Controller

---
+++ src/main/java/com/aya/spring/springdemomaven/web/StackoverflowController.java
@@ -0,0 +1,22 @@
+package com.aya.spring.springdemomaven.web;
+
+import com.aya.spring.springdemomaven.model.StackoverflowWebsite;
+import com.aya.spring.springdemomaven.service.StackoverflowService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/stackoverflow")
+public class StackoverflowController {
+
+    @Autowired
+    private StackoverflowService stackoverflowService;
+
+    @RequestMapping
+    public List<StackoverflowWebsite> getListOfProviders(){
+        return stackoverflowService.findAll();
+    }
+}
